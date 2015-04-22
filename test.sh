3c2af3899e3000d3ff6c60bf4be1247ef33e08c7
Brian Anderson <banderson@mozilla.com>
2015-04-22 11:00:49 -0700

    Use correct conventions

--- test.sh
+++ test.sh
@@ -4,11 +4,11 @@ set -e -u
 
 # Prints the absolute path of a directory to stdout
 abs_path() {
-    local path="$1"
+    local _path="$1"
     # Unset CDPATH because it causes havok: it makes the destination unpredictable
     # and triggers 'cd' to print the path to stdout. Route `cd`'s output to /dev/null
     # for good measure.
-    (unset CDPATH && cd "$path" > /dev/null && pwd)
+    (unset CDPATH && cd "$_path" > /dev/null && pwd)
 }
 
 S="$(abs_path $(dirname $0))"
