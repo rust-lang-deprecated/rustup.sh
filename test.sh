8244f5c7f62aec91ec1a033ab26e918640ac066a
Brian Anderson <banderson@mozilla.com>
2015-10-15 15:47:25 -0700

    Exit test.sh on failure

--- test.sh
+++ test.sh
@@ -8,10 +8,10 @@ echo
 echo "Running v1 metadata test suite"
 echo
 
-"$S/test-v1.sh"
+"$S/test-v1.sh" || return 1
 
 echo
 echo "Running v2 metadata test suite"
 echo
 
-"$S/test-v2.sh"
+"$S/test-v2.sh" || return 1
